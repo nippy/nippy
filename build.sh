04e7322df05e8be5f99acf0d122b8442c22522b1
Torkild Dyvik Olsen <torkild@webology.no>
2016-12-01 16:23:02 +0100

    Adding step to replace placeholders in build process.

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 set -e -o pipefail
-
 cd `dirname $0`
-SRC_DIR=./src
-DIST_DIR=./dist
 
 TSC=$(npm bin)/tsc
 
@@ -13,14 +10,11 @@ VERSION_SUFFIX="-$(git log --oneline --color=never -1 | awk '{print $1}')"
 VERSION=${VERSION_PREFIX}${VERSION_SUFFIX}
 
 echo "##### BUILDING: Version ${VERSION}"
+rm -rf ./dist/
 
-# Delete dist folder
-rm -rf ${DIST_DIR}
-
-# Compile all source
 echo "===== ALL:"
-echo "    - Compiling: \$(npm bin)/tsc -p ${SRC_DIR}/tsconfig.json"
-$TSC -p ${SRC_DIR}/tsconfig.json
+echo "    - Compiling: \$(npm bin)/tsc -p ./src/tsconfig.json"
+$TSC -p ./src/tsconfig.json
 
 MODULES=(
 	config
@@ -32,15 +26,25 @@ MODULES=(
 for MODULE in "${MODULES[@]}"
 do
 	echo "===== MODULE: ${MODULE}"
-	MODULE_DIR=${SRC_DIR}/${MODULE}
-	DEST_DIR=${DIST_DIR}/package-dist/${MODULE}
+	SRC_DIR=./src/${MODULE}
+	DEST_DIR=./dist/packages-dist/${MODULE}
 
-	# Compile TS
 	echo "    - Compiling: \$(npm bin)/tsc -p ${SRC_DIR}/tsconfig-build.json"
-	$TSC -p ${MODULE_DIR}/tsconfig-build.json
+	$TSC -p ${SRC_DIR}/tsconfig-build.json
 
-	# Copy static files
-	cp ${MODULE_DIR}/package.json ${DEST_DIR}/
+	echo "    - Copying static files"
+	cp ${SRC_DIR}/package.json ${DEST_DIR}/
 	cp ./LICENSE ${DEST_DIR}/
 	cp ./README.md ${DEST_DIR}/ # TODO: Use module readme if found.
 done
+
+echo "===== Replacing placeholders"
+AUTHOR=$(node -p "require('./package.json').author" | sed -e 's/[\/&]/\\&/g')
+LICENSE=$(node -p "require('./package.json').license")
+REPO_URL=$(node -p "require('./package.json').repository.url" | sed -e 's/[\/&]/\\&/g')
+find ./dist/packages-dist/ -type f -name package.json -print0 | xargs -0 sed -i "s/PLACEHOLDER-VERSION/${VERSION_PREFIX}/g"
+find ./dist/packages-dist/ -type f -name package.json -print0 | xargs -0 sed -i "s/PLACEHOLDER-AUTHOR/${AUTHOR}/g"
+find ./dist/packages-dist/ -type f -name package.json -print0 | xargs -0 sed -i "s/PLACEHOLDER-LICENSE/${LICENSE}/g"
+find ./dist/packages-dist/ -type f -name package.json -print0 | xargs -0 sed -i "s/PLACEHOLDER-REPO-URL/${REPO_URL}/g"
+
+echo "##### DONE"
